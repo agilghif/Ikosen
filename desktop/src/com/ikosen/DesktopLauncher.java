5c36389d6ad855b410c2960e0c385ec9331e9765
Muhammad Agil Ghifari <muhammad.agil01@ui.ac.id>
2023-01-13 14:13:32 +0700

    Changing file IkosenInterface class name to Ikosen

--- desktop/src/com/ikosen/DesktopLauncher.java
+++ desktop/src/com/ikosen/DesktopLauncher.java
@@ -2,15 +2,14 @@ package com.ikosen;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.ikosen.IkosenInterface;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowSizeLimits(600,600,600,600);
+		config.setWindowSizeLimits(720,720,720,720);
 		config.setTitle("Ikosen");
-		new Lwjgl3Application(new IkosenInterface(), config);
+		new Lwjgl3Application(new Ikosen(), config);
 	}
 }
